ac74ae6cfca1aa7fd7dc6552caa2d27023721b98
jadamcrain <jadamcrain@gmail.com>
2017-10-17 11:49:10 -0700

    require COREDX_TARGET to be specified or set via environment

--- cmake/coredx.cmake
+++ cmake/coredx.cmake
@@ -1,10 +1,10 @@
 if(NOT DEFINED COREDX_TOP)	
-	# require that the COREDX_TOP environment variable be set
+	# try to find via COREDX_TOP environment variable instead
 	if(DEFINED ENV{COREDX_TOP})
 		message("COREDX_TOP found via environment variable")
 		set(COREDX_TOP $ENV{COREDX_TOP})
 	else()   
-		message(FATAL_ERROR "You must define COREDX_TOP when configuring or via environment")
+		message(FATAL_ERROR "You must define COREDX_TOP when configuring cmake or via environment")
 	endif()
 endif()
 
@@ -16,12 +16,19 @@ if(NOT EXISTS ${COREDX_INCLUDE}/dds/dds.hh)
 	message(FATAL_ERROR "Unable to find dds.hh in ${COREDX_INCLUDE}")
 endif()
 
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
-	set(COREDX_TARGET Windows_x86_vs2012)
-elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
-	set(COREDX_TARGET Linux_2.6_x86_64_gcc43)
-else()
-	message(FATAL_ERROR "This script doesn't support ${CMAKE_CXX_COMPILER_ID} yet")
+if(NOT DEFINED COREDX_TARGET)
+  	# try to find via COREDX_TARGET environment variable instead
+	if(DEFINED ENV{COREDX_TARGET})
+		message("COREDX_TARGET found via environment variable")
+		set(COREDX_TARGET $ENV{COREDX_TARGET})
+	else()   
+		message(FATAL_ERROR "You must define COREDX_TARGET when configuring cmake or via environment")
+	endif()
 endif()
 
-set(COREDX_LIB_DIR ${CORE_DX_TOP}/target/${COREDX_TARGET}/lib)
+message("COREDX_TARGET is ${COREDX_TARGET}")
+
+set(COREDX_LIB_DIR "${COREDX_TOP}/target/${COREDX_TARGET}/lib")
+if(NOT EXISTS ${COREDX_LIB_DIR})
+	message(FATAL_ERROR "Target lib dir doesn't exist: ${COREDX_LIB_DIR}")
+endif()
