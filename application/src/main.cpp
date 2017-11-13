39b32c3fa33563e66e2f736bac1f773b13caa1ba
jadamcrain <jadamcrain@gmail.com>
2017-11-13 12:19:31 -0800

    start reworking YAML based around new architecture

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -5,23 +5,35 @@
 #include "ProtoBus.h"
 #include "dnp3/DNP3Factory.h"
 
+#include <fstream>
+#include <iostream>
+
 using namespace openfmb;
 
-int main()
+int main(int argc, char** argv)
 {
-    /**
-     * Initialize the factories for the adapters.
-     */
+    if(argc != 2) {
+        std::cerr << "usage: " << std::endl;
+        std::cerr << "openfmb-adapter <configuration.yaml>" << std::endl;
+        return -1;
+    }
+
+    std::ifstream fin(argv[1]);
+    YAML::Parser parser(fin);
+
+    YAML::Node doc;
+    parser.PrintTokens(std::cout);
+    return 0;
+
+
+    // Initialize the factories for the adapters.
     std::map<std::string, std::unique_ptr<IAdapterFactory>> factories;
     factories["dnp3"] = std::make_unique<DNP3Factory>();
-
-    // TODO - load the YAML and create the adapters
-
 
     // adapters publish and subscribe to this common bus
     const auto bus = std::make_shared<ProtoBus>();
 
-    // TODO - have plugins publish and subscribe to the bus
+    // TODO - have plugins publish and subscribe to the bus using the YAML
 
     // don't allow any more subscriptions to the bus (effectively make it immutable)
     bus->finalize();
