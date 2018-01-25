904426a38fbe24ed620580619007b346d734d33c
jadamcrain <jadamcrain@gmail.com>
2018-01-25 11:57:13 -0800

    Add empty file for IProtoVisitor

---
+++ adapter-api/src/adapter-api/helpers/IProtoVisitor.h
@@ -0,0 +1,17 @@
+
+#ifndef OPENFMB_ADAPTER_IPROTOVISITOR_H
+#define OPENFMB_ADAPTER_IPROTOVISITOR_H
+
+namespace adapter {
+
+    /**
+     * A callback class used to traverse an openFMB protocol buffer hierarchy
+     *
+     * This is used for both generating default configurations and reading configurations
+     */
+    class IProtoVisitor {
+
+    };
+}
+
+#endif //OPENFMB_ADAPTER_IPROTOVISITOR_H
