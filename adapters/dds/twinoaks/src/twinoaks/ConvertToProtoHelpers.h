2410a74efc3efa401be18de7c3ff6b41879340d7
jadamcrain <jadamcrain@gmail.com>
2018-02-15 10:50:21 -0800

    working generation of DDS to proto conversion routines

---
+++ adapters/dds/twinoaks/src/twinoaks/ConvertToProtoHelpers.h
@@ -0,0 +1,86 @@
+#ifndef OPENFMB_ADAPTER_CONVERTTOPROTOHELPERS_H
+#define OPENFMB_ADAPTER_CONVERTTOPROTOHELPERS_H
+
+namespace adapter
+{
+
+    namespace dds
+    {
+
+        template <class Out, class In>
+        void convert_repeated_field(const DDS::sequence<In>& input, ::google::protobuf::RepeatedPtrField<Out>& output)
+        {
+            output.Clear();
+            for(decltype(input.length()) i=0; i<input.length(); ++i)
+            {
+                convert_to_proto(input.at(i), *output.Add());
+            }
+        };
+
+
+        // -------- TODO ------------
+
+        inline char* convert_string(char* str)
+        {
+            return str; // nop
+        }
+
+        inline bool convert_bool(bool value)
+        {
+            return value; // nop
+        }
+
+        // -------- numeric conversions ---------
+
+        inline ::google::protobuf::int32 convert_int32(openfmb::commonmodule::INT32 value)
+        {
+            return static_cast<::google::protobuf::int32>(value);
+        }
+
+        inline ::google::protobuf::uint32 convert_uint32(openfmb::commonmodule::INT32U value)
+        {
+            return static_cast<::google::protobuf::uint32>(value);
+        }
+
+        inline ::google::protobuf::uint64 convert_uint64(openfmb::commonmodule::INT64U value)
+        {
+            return static_cast<::google::protobuf::uint64>(value);
+        }
+
+        inline openfmb::commonmodule::INT64 convert_int64(::google::protobuf::int64 value)
+        {
+            return static_cast<openfmb::commonmodule::INT64>(value);
+        }
+
+        inline openfmb::commonmodule::FLOAT32 convert_float(float value)
+        {
+            return static_cast<openfmb::commonmodule::FLOAT32>(value);
+        }
+
+
+        // -------- enum conversions ---------
+
+        inline ::commonmodule::UnitSymbolKind convert_enum(openfmb::commonmodule::UnitSymbolKind value)
+        {
+            return static_cast<::commonmodule::UnitSymbolKind>(value);
+        }
+
+        inline ::commonmodule::CalcMethodKind convert_enum(openfmb::commonmodule::CalcMethodKind value)
+        {
+            return static_cast<::commonmodule::CalcMethodKind>(value);
+        }
+
+        inline ::commonmodule::PhaseCodeKind convert_enum(openfmb::commonmodule::PhaseCodeKind value)
+        {
+            return static_cast<::commonmodule::PhaseCodeKind>(value);
+        }
+
+        inline ::commonmodule::UnitMultiplierKind convert_enum(openfmb::commonmodule::UnitMultiplierKind value)
+        {
+            return static_cast<::commonmodule::UnitMultiplierKind>(value);
+        }
+
+    }
+}
+
+#endif //OPENFMB_ADAPTER_CONVERSIONHELPERS_H
--- codegen/src/main/java/com/oes/openfmb/generation/dds/ConvertToProto.java
+++ codegen/src/main/java/com/oes/openfmb/generation/dds/ConvertToProto.java
@@ -50,8 +50,8 @@ public class ConvertToProto extends CppFilePair {
                 FileHeader.lines,
                 include(this.baseFileName() + ".h"),
                 space,
-                //include("../ConvertFromProtoHelpers.h"),
-                //space,
+                include("../ConvertToProtoHelpers.h"),
+                space,
                 namespace("adapter",
                         namespace("dds",
                                 implementations()
@@ -69,8 +69,135 @@ public class ConvertToProto extends CppFilePair {
     {
         return line(signature((d)))
                 .append("{")
+                .indent(
+                        FieldInfo.omitConversion(d) ? line("// omitted via configuration") : join(
+                                line("out.Clear();"),
+                                messageFieldConversions(d),
+                                primitiveFieldConversions(d)
+                        )
+                )
                 .append("}");
 
+    }
+
+
+    private Document messageFieldConversions(Descriptors.Descriptor d)
+    {
+        Document conversions = join(
+                d.getFields().stream().map(this::messageFieldConversion)
+        );
+
+        return conversions.isEmpty() ? conversions : line("// convert message fields").append(conversions);
+    }
+
+    private Document primitiveFieldConversions(Descriptors.Descriptor d) {
+
+        Document conversions = join(
+                d.getFields().stream().map(this::primitiveFieldConversion)
+        );
+
+        return conversions.isEmpty() ? conversions : line("// convert primitive fields").append(conversions);
+    }
+
+    private Document primitiveFieldConversion(Descriptors.FieldDescriptor field)
+    {
+        if(field.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) return Documents.empty;
+
+        if(field.getType() == Descriptors.FieldDescriptor.Type.BYTES)
+        {
+            return line(String.format("// omitting 'bytes' field called %s", field.getName()));
+        }
+
+        if(FieldInfo.isRequired(field))
+        {
+            return line(
+                    String.format("out.set_%s(convert_%s(in.%s));",
+                            field.getName().toLowerCase(),
+                            field.getType().toString().toLowerCase(),
+                            field.getName()
+                    )
+            );
+        }
+        else
+        {
+            if(field.getType().equals(Descriptors.FieldDescriptor.Type.STRING))
+            {
+                return line(
+                        String.format(
+                                "if(in.%s) out.set_%s(in.%s);",
+                                field.getName(),
+                                field.getName().toLowerCase(),
+                                field.getName()
+                        )
+                );
+            }
+            else
+            {
+                return line(
+                        String.format(
+                                "if(in.%s) out.set_%s(convert_%s(*in.%s));",
+                                field.getName(),
+                                field.getName().toLowerCase(),
+                                field.getType().toString().toLowerCase(),
+                                field.getName()
+                        )
+                );
+            }
+        }
+
+    }
+
+    private Document messageFieldConversion(Descriptors.FieldDescriptor field)
+    {
+        if(field.getType() != Descriptors.FieldDescriptor.Type.MESSAGE) return Documents.empty;
+
+        if(FieldInfo.isInherited(field))
+        {
+            return line(
+                    String.format(
+                            "convert_to_proto(in, *out.mutable_%s()); // inherited type",
+                            field.getName().toLowerCase()
+                    )
+            );
+        }
+        else
+        {
+            if(field.isRepeated())
+            {
+                return line(
+                        String.format(
+                                "convert_repeated_field(in.%s, *out.mutable_%s()); // repeated field",
+                                field.getName(),
+                                field.getName().toLowerCase()
+                        )
+                );
+            }
+            else
+            {
+                if(FieldInfo.isRequired(field))
+                {
+                    return line(
+                            String.format(
+                                    "convert_to_proto(in.%s, *out.mutable_%s()); // required field in DDS",
+                                    field.getName(),
+                                    field.getName().toLowerCase()
+                            )
+                    );
+                }
+                else
+                {
+                    return line(
+                            String.format(
+                                    "if(in.%s) convert_to_proto(*in.%s, *out.mutable_%s());",
+                                    field.getName(),
+                                    field.getName(),
+                                    field.getName().toLowerCase()
+                            )
+                    );
+                }
+            }
+
+        }
     }
 
     private Document headerIncludes()
@@ -97,11 +224,6 @@ public class ConvertToProto extends CppFilePair {
     private static String signature(Descriptors.Descriptor d)
     {
         return String.format("void convert_to_proto(const openfmb::%s& in, %s& out)", cppName(d), cppName(d));
-    }
-
-    private static String cppName(Descriptors.EnumDescriptor d)
-    {
-        return d.getFullName().replace(".", "::");
     }
 
     private static String cppName(Descriptors.Descriptor d)
