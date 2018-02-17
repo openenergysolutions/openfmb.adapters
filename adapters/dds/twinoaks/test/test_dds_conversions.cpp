1b502b986836053d250b0f1ba52ba404cca1eecc
jadamcrain <jadamcrain@gmail.com>
2018-02-17 09:22:10 -0800

    test cases linking correctly

---
+++ adapters/dds/twinoaks/test/test_dds_conversions.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+
+#include <adapter-api/proto/resourcemodule/resourcemodule.pb.h>
+
+#include "twinoaks/generated/ConvertFromProto.h"
+#include "twinoaks/generated/ConvertToProto.h"
+
+TEST_CASE( "protobuf <=> DDS conversions round trip correctly" ) {
+
+}
