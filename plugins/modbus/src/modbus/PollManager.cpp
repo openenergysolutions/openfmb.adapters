3ca70f2e2f4d2c6e41150df61d3d2f4fe55e77ea
jadamcrain <jadamcrain@gmail.com>
2018-03-25 14:34:02 -0700

    first pass at a complete PollManager that performs scans and publishes

--- plugins/modbus/src/modbus/PollManager.cpp
+++ plugins/modbus/src/modbus/PollManager.cpp
@@ -1,4 +1,5 @@
 
+
 #include "PollManager.h"
 
 namespace adapter
@@ -6,22 +7,84 @@ namespace adapter
 
     namespace modbus
     {
+        template <class R, class T>
+        void PollManager::add_poll(const T& request)
+        {
+            auto poll = [request](const std::shared_ptr<PollManager>& manager)
+            {
+
+                auto handler = [manager](const ::modbus::Expected<R>& result)
+                {
+
+                    if(result.is_valid())
+                    {
+                        manager->sink->apply(result.get());
+                        manager->on_poll_success();
+                    }
+                    else
+                    {
+                        manager->on_poll_failure();
+                    }
+                };
+
+                manager->session->send_request(request, handler);
+            };
+
+            this->polls.push_back(poll);
+        }
+
 
         PollManager::PollManager(const Logger& logger,
                                  std::unique_ptr<IPollSink> sink,
                                  const std::chrono::steady_clock::duration& period,
-                                 const std::shared_ptr<::modbus::ISession>& session) :
+                                 const session_t& session) :
             logger(logger),
             sink(std::move(sink)),
             period(period),
             session(session),
-            timer(nullptr) // TODO - Is this correct?
+            timer(nullptr) // TODO - correct?
         {}
+
+        void PollManager::add(const ::modbus::ReadHoldingRegistersRequest& request)
+        {
+            this->add_poll<::modbus::ReadHoldingRegistersResponse>(request);
+        }
 
         void PollManager::start()
         {
-
+            if(!this->polls.empty())
+            {
+                this->poll_sequence.clear();
+                for(auto& poll : this->polls) this->poll_sequence.push_back(poll);
+                this->poll_sequence_start = std::chrono::steady_clock::now();
+                this->sink->begin();
+                this->on_poll_success();
+            }
         }
+
+        void PollManager::on_poll_success()
+        {
+            if(this->poll_sequence.empty())
+            {
+                this->sink->end();
+                this->timer = this->session->start(period, [self = this->shared_from_this()]()
+                {
+                    self->start();
+                });
+            }
+        }
+
+        void PollManager::on_poll_failure()
+        {
+            this->logger.warn("Poll sequence failed");
+            this->poll_sequence.clear();
+            // TODO - should we have a try parameter that's distinct from the period?
+            this->timer = this->session->start(period, [self = this->shared_from_this()]()
+            {
+                self->start();
+            });
+        }
+
     }
 }
 
--- plugins/modbus/src/modbus/PollManager.h
+++ plugins/modbus/src/modbus/PollManager.h
@@ -1,41 +1,52 @@
 
-#ifndef OPENFMB_ADAPTER_MODBUS_POLLMANAGER_H
-#define OPENFMB_ADAPTER_MODBUS_POLLMANAGER_H
+#ifndef OPENFMB_ADAPTER_MODBUS_POLLBUILDER_H
+#define OPENFMB_ADAPTER_MODBUS_POLLBUILDER_H
+
+
+#include "adapter-api/Logger.h"
 
 #include "IPollSink.h"
 
 #include "modbus/session/ISession.h"
-
-#include "adapter-api/Logger.h"
-
-#include <chrono>
+#include "modbus/exceptions/IException.h"
 
 namespace adapter
 {
-
     namespace modbus
     {
-
-        class PollManager
+        class PollManager : public std::enable_shared_from_this<PollManager>
         {
+            using session_t = std::shared_ptr<::modbus::ISession>;
+            using poll_action_t = std::function<void (const std::shared_ptr<PollManager>&)>;
 
         public:
 
             PollManager(const Logger& logger,
                         std::unique_ptr<IPollSink> sink,
                         const std::chrono::steady_clock::duration& period,
-                        const std::shared_ptr<::modbus::ISession>& session);
+                        const session_t& session);
+
+            void add(const ::modbus::ReadHoldingRegistersRequest& request);
 
             void start();
 
         private:
 
+            void on_poll_success();
+
+            void on_poll_failure();
+
+            template <class R, class T>
+            void add_poll(const T& request);
+
             Logger logger;
             const std::unique_ptr<IPollSink> sink;
             const std::chrono::steady_clock::duration period;
-            const std::shared_ptr<::modbus::ISession> session;
-
+            const session_t session;
             loopser::Timer timer;
+            std::chrono::steady_clock::time_point poll_sequence_start;
+            std::vector<poll_action_t> polls;
+            std::vector<poll_action_t> poll_sequence;
         };
 
     }
