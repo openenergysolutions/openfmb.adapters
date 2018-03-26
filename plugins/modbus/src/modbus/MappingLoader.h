6f9fffc03cc69d22992ed8630ad5181ab8a8b8fa
jadamcrain <jadamcrain@gmail.com>
2018-03-26 07:15:49 -0700

    Map BCR values in Modbus

--- plugins/modbus/src/modbus/MappingLoader.h
+++ plugins/modbus/src/modbus/MappingLoader.h
@@ -39,30 +39,6 @@ namespace adapter
 
             void handle(const std::string& field_name, getter_t<commonmodule::CMV, T> getter) override
             {
-                this->configure(field_name, getter);
-            }
-
-            void handle(const std::string& field_name, getter_t<commonmodule::BCR, T> getter) override {}
-
-            void handle(const std::string& field_name, getter_t<commonmodule::ReadingMessageInfo, T> getter) override {}
-
-            void handle(const std::string& field_name, getter_t<commonmodule::IdentifiedObject, T> getter) override {}
-
-            void handle(const std::string& field_name, getter_t<commonmodule::ConductingEquipmentTerminalReading, T> getter) override {}
-
-            void handle(const std::string& field_name, getter_t<commonmodule::LogicalNode, T> getter) override {}
-
-            void handle(const std::string& field_name, getter_t<commonmodule::ENG_CalcMethodKind, T> getter) override {}
-
-        private:
-
-            static double get_scale(const YAML::Node& node)
-            {
-                return yaml::require(node, keys::scale).as<double>();
-            }
-
-            void configure(const std::string& field_name, getter_t<commonmodule::CMV, T> getter)
-            {
                 const YAML::Node vector_node = yaml::require(
                                                    this->get_config_node(field_name),
                                                    keys::cVal
@@ -87,8 +63,80 @@ namespace adapter
                 );
             }
 
+            void handle(const std::string& field_name, getter_t<commonmodule::BCR, T> getter) override
+            {
+                const auto node = this->get_config_node(field_name);
+
+                const auto type = MappingTypeMeta::from_string(yaml::require_string(node, keys::type));
+                switch(type)
+                {
+                case(MappingType::bit16):
+                    this->map_bcr_bit16(node, getter);
+                    break;
+                case(MappingType::bit32):
+                    this->map_bcr_bit32(node, getter);
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            void handle(const std::string& field_name, getter_t<commonmodule::ReadingMessageInfo, T> getter) override {}
+
+            void handle(const std::string& field_name, getter_t<commonmodule::IdentifiedObject, T> getter) override {}
+
+            void handle(const std::string& field_name, getter_t<commonmodule::ConductingEquipmentTerminalReading, T> getter) override {}
+
+            void handle(const std::string& field_name, getter_t<commonmodule::LogicalNode, T> getter) override {}
+
+            void handle(const std::string& field_name, getter_t<commonmodule::ENG_CalcMethodKind, T> getter) override {}
+
+        private:
+
+            void map_bcr_bit16(const YAML::Node& node, getter_t<commonmodule::BCR, T> getter)
+            {
+                const auto index = yaml::require(node, keys::index).as<uint16_t>();
+
+                const auto reg = std::make_shared<Register16>();
+                this->mapping.add_holding_register(index, reg);
+                this->add_bcr_init_and_flush(reg, getter);
+            }
+
+            void map_bcr_bit32(const YAML::Node& node, getter_t<commonmodule::BCR, T> getter)
+            {
+                const auto lower_index = yaml::require(node, keys::lower_index).as<uint16_t>();
+                const auto upper_index = yaml::require(node, keys::upper_index).as<uint16_t>();
+
+                const auto reg = std::make_shared<Register32>();
+
+                this->mapping.add_holding_register(lower_index, reg->get_lower());
+                this->mapping.add_holding_register(upper_index, reg->get_upper());
+
+                this->add_bcr_init_and_flush(reg, getter);
+            }
+
+            void add_bcr_init_and_flush(const std::shared_ptr<ICachedValue>& value, getter_t<commonmodule::BCR, T> getter)
+            {
+                this->mapping.add_init_action([value]()
+                {
+                    value->reset();
+                });
+
+                this->mapping.add_flush_action(
+                    [value, getter](T & profile) -> bool
+                {
+                    if(value->is_set())
+                    {
+                        getter(profile)->set_actval(value->to_int64());
+                        return true;
+                    }
+                    return false;
+                }
+                );
+            }
+
             template <class F>
-            void configure_analogue(const YAML::Node& node, const F& getter)
+            void configure_analogue(const YAML::Node& node, F getter)
             {
                 const auto type = MappingTypeMeta::from_string(yaml::require_string(node, keys::type));
                 switch(type)
@@ -105,7 +153,7 @@ namespace adapter
             }
 
             template <class F>
-            void map_analogue_bit16(const YAML::Node& node, const F& getter)
+            void map_analogue_bit16(const YAML::Node& node, F getter)
             {
                 const auto scale = yaml::require(node, keys::scale).as<float>();
                 const auto index = yaml::require(node, keys::index).as<uint16_t>();
@@ -117,7 +165,7 @@ namespace adapter
             }
 
             template <class F>
-            void map_analogue_bit32(const YAML::Node& node, const F& getter)
+            void map_analogue_bit32(const YAML::Node& node, F getter)
             {
                 const auto scale = yaml::require(node, keys::scale).as<float>();
                 const auto upper_index = yaml::require(node, keys::upper_index).as<uint16_t>();
@@ -131,7 +179,7 @@ namespace adapter
             }
 
             template <class F>
-            void add_analogue_init_and_flush(const std::shared_ptr<ICachedValue>& value, float scale, const F& getter)
+            void add_analogue_init_and_flush(const std::shared_ptr<ICachedValue>& value, float scale, F getter)
             {
                 this->mapping.add_init_action([value]()
                 {
