c52a3cb804557ccb05e6e5db24ef1871b141f4b7
jadamcrain <jadamcrain@gmail.com>
2018-05-20 17:03:38 -0700

    create callback interface for extracting measurement values, create template specializations for extract messageinfo/conductingequip

---
+++ adapter-api/src/adapter-api/config/MessageInformation.cpp
@@ -0,0 +1,66 @@
+
+#include "MessageInformation.h"
+
+#include "../proto/resourcemodule/resourcemodule.pb.h"
+#include "../proto/switchmodule/switchmodule.pb.h"
+
+namespace adapter {
+
+    // resource reading
+
+    template <>
+    const commonmodule::MessageInfo& get_message_info(const resourcemodule::ResourceReadingProfile& profile)
+    {
+        return profile.readingmessageinfo().messageinfo();
+    }
+
+    template <>
+    const commonmodule::ConductingEquipment& get_conducting_equip(const resourcemodule::ResourceReadingProfile& profile)
+    {
+        return profile.meter().conductingequipment();
+    }
+
+    // switch reading
+
+    template <>
+    const commonmodule::MessageInfo& get_message_info(const switchmodule::SwitchReadingProfile& profile)
+    {
+        return profile.readingmessageinfo().messageinfo();
+    }
+
+    template <>
+    const commonmodule::ConductingEquipment& get_conducting_equip(const switchmodule::SwitchReadingProfile& profile)
+    {
+        return profile.protectedswitch().conductingequipment();
+    }
+
+    // switch status
+
+    template <>
+    const commonmodule::MessageInfo& get_message_info(const switchmodule::SwitchStatusProfile& profile)
+    {
+        return profile.statusmessageinfo().messageinfo();
+    }
+
+    template <>
+    const commonmodule::ConductingEquipment& get_conducting_equip(const switchmodule::SwitchStatusProfile& profile)
+    {
+        return profile.protectedswitch().conductingequipment();
+    }
+
+    // switch control
+
+    template <>
+    const commonmodule::MessageInfo& get_message_info(const switchmodule::SwitchControlProfile& profile)
+    {
+        return profile.controlmessageinfo().messageinfo();
+    }
+
+    template <>
+    const commonmodule::ConductingEquipment& get_conducting_equip(const switchmodule::SwitchControlProfile& profile)
+    {
+        return profile.protectedswitch().conductingequipment();
+    }
+}
+
+
