ec2bae4a29a2b7916be821585f6d8cf1d72b7bcf
jadamcrain <jadamcrain@gmail.com>
2018-07-09 15:30:38 -0700

    Add an interface for an un-templated model visitor, and use a base class for methods it shares with the templated version

---
+++ adapter-api/include/adapter-api/config/IModelVisitor.h
@@ -0,0 +1,25 @@
+
+#ifndef OPENFMB_ADAPTER_IMODELVISITOR_H
+#define OPENFMB_ADAPTER_IMODELVISITOR_H
+
+#include "IMessageFieldHandler.h"
+
+namespace adapter {
+
+class IModelVisitor : public IMessageFieldHandler {
+
+public:
+    // --- handlers for the various primitive types ---
+
+    virtual void handle_bool(const std::string& field_name) = 0;
+    virtual void handle_int32(const std::string& field_name) = 0;
+    virtual void handle_uint32(const std::string& field_name) = 0;
+    virtual void handle_int64(const std::string& field_name) = 0;
+    virtual void handle_uint64(const std::string& field_name) = 0;
+    virtual void handle_float(const std::string& field_name) = 0;
+    virtual void handle_string(const std::string& field_name) = 0;
+    virtual void handle_enum(const std::string& field_name, google::protobuf::EnumDescriptor const* descriptor) = 0;
+};
+}
+
+#endif
--- adapter-api/include/adapter-api/config/ITypedModelVisitor.h
+++ adapter-api/include/adapter-api/config/ITypedModelVisitor.h
@@ -3,44 +3,17 @@
 #define OPENFMB_ADAPTER_ITYPEDMODELVISITOR_H
 
 #include "IAccessor.h"
+#include "IMessageFieldHandler.h"
 
 #include <google/protobuf/descriptor.h>
 #include <string>
 
 namespace adapter {
+
 template <class P>
-class ITypedModelVisitor {
+class ITypedModelVisitor : public IMessageFieldHandler {
 
 public:
-    /**
-         * start traversing a message field
-         *
-         * @param field_name the name of the field inside the message
-         * @param descriptor Protobuf message descriptor for the referenced field
-         *
-         * @return true if the field should be traversed, false to terminate traversal
-         */
-    virtual bool start_message_field(const std::string& field_name, google::protobuf::Descriptor const* descriptor) = 0;
-
-    /**
-         * Start traversing a repeated message field
-         *
-         * @param field_name the name of the field inside the message
-         * @param descriptor Protobuf message descriptor for the referenced field
-         * @return number of times to traverse the repeated field
-         */
-    virtual int start_repeated_message_field(const std::string& field_name, google::protobuf::Descriptor const* descriptor) = 0;
-
-    virtual void start_iteration(int i) = 0;
-
-    virtual void end_iteration() = 0;
-
-    /**
-         * Called when a previously started message/repeated field iteration completes. Called even
-         * if start_message_field(..) returned false or start_repeated_message_field returns 0
-         */
-    virtual void end_message_field() = 0;
-
     // --- handlers for the various primitive types ---
 
     virtual void handle(const std::string& field_name, const accessor_t<P, bool>& setter) = 0;
