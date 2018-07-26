2b18c10262cb38c6a61fad9a4933b2194c912170
jadamcrain <jadamcrain@gmail.com>
2018-07-26 15:59:50 -0700

    create a base class for configuration reading for control profiles that will be used by DNP3 & Modbus

--- plugins/modbus/src/modbus/CommandSinkActions.h
+++ plugins/modbus/src/modbus/CommandSinkActions.h
@@ -6,117 +6,120 @@
 #include "ICommandConfigBuilder.h"
 #include "ICommandPrioritySource.h"
 
-#include "generated/RegisterOperation.h"
 #include "generated/OutputType.h"
+#include "generated/RegisterOperation.h"
 
 #include <yaml-cpp/yaml.h>
 
 #include <functional>
 
-namespace adapter
-{
-    namespace modbus
-    {
-        using sink_action_t = std::function<void (ICommandSink&)>;
+namespace adapter {
+namespace modbus {
+    using sink_action_t = std::function<void(ICommandSink&)>;
 
-        class BinaryConfigPair
+    class BinaryConfigPair {
+        std::vector<sink_action_t> when_true;
+        std::vector<sink_action_t> when_false;
+
+    public:
+        BinaryConfigPair(std::vector<sink_action_t> when_true, std::vector<sink_action_t> when_false)
+            : when_true(std::move(when_true))
+            , when_false(std::move(when_false))
         {
-            std::vector<sink_action_t> when_true;
-            std::vector<sink_action_t> when_false;
+        }
 
-        public:
-            BinaryConfigPair(std::vector<sink_action_t> when_true, std::vector<sink_action_t> when_false)
-                    : when_true(std::move(when_true))
-                    , when_false(std::move(when_false))
-            {}
-
-            bool is_empty() const
-            {
-                return when_true.empty() && when_false.empty();
-            }
-
-            bool apply(bool value, ICommandSink& sink) const
-            {
-                auto ret = false;
-                for(const auto& action : value ? when_true : when_false)
-                {
-
-                    action(sink);
-                    ret = true;
-                }
-                return ret;
-            }
-        };
-
-        namespace read
+        bool is_empty() const
         {
-            sink_action_t single_register_command_sink_action(const YAML::Node& node, const ICommandPrioritySource& priority_source)
-            {
-                const auto operation = yaml::require_enum<RegisterOperation>(node);
-                const auto index = ::adapter::yaml::get::index(node);
-                const auto priority = priority_source.get_priority(CommandType::Value::write_single_register, index);
-                switch(operation)
-                {
-                    case(RegisterOperation::Value::clear_masked_bits):
-                        return [ =, mask = yaml::require_integer<uint16_t>(node, keys::mask)](ICommandSink & sink)
-                        {
-                            return sink.modify_register(index, priority, operations::clear(mask));
-                        };
-                    case(RegisterOperation::Value::set_masked_bits):
-                        return [ =, mask = yaml::require_integer<uint16_t>(node, keys::mask)](ICommandSink & sink)
-                        {
-                            return sink.modify_register(index, priority, operations::set(mask));
-                        };
-                    case(RegisterOperation::Value::write_value):
-                        return [ =, value = yaml::require_integer<uint16_t>(node, ::adapter::keys::value)](ICommandSink & sink)
-                        {
-                            return sink.set_register(index, priority, value);
-                        };
-                    default:
-                        throw Exception("Unsupported register operation type: ", RegisterOperation::to_string(operation));
-                }
+            return when_true.empty() && when_false.empty();
+        }
+
+        bool apply(bool value, ICommandSink& sink) const
+        {
+            auto ret = false;
+            for (const auto& action : value ? when_true : when_false) {
+
+                action(sink);
+                ret = true;
             }
+            return ret;
+        }
+    };
 
-            std::vector<sink_action_t> sink_actions(const YAML::Node& node, const ICommandPrioritySource& priority_source)
-            {
-                std::vector<sink_action_t> actions;
+    namespace read {
 
-                yaml::foreach(
-                        yaml::require(node, ::adapter::keys::outputs),
-                        [&actions, &priority_source](const YAML::Node& node)
-                        {
-                            const auto output_type = yaml::require_enum<OutputType>(node);
-                            switch(output_type)
-                            {
-                                case(OutputType::Value::none):
-                                    break;
-                                case(OutputType::Value::write_single_register):
-                                    actions.push_back(single_register_command_sink_action(node, priority_source));
-                                    break;
-                                default:
-                                    throw Exception("Unsupported output type: ", OutputType::to_string(output_type));
-                            }
-                        }
-                );
-
-                return std::move(actions);
-            }
-
-            static BinaryConfigPair binary_action_pair(const YAML::Node& node, const ICommandPrioritySource& priority_source)
-            {
-                return BinaryConfigPair
-                        {
-                                std::move(
-                                        sink_actions(yaml::require(node, ::adapter::keys::when_true), priority_source)
-                                ),
-                                std::move(
-                                        sink_actions(yaml::require(node, ::adapter::keys::when_false), priority_source)
-                                )
-                        };
+        sink_action_t single_register_command_sink_action(const YAML::Node& node, const ICommandPrioritySource& priority_source)
+        {
+            const auto operation = yaml::require_enum<RegisterOperation>(node);
+            const auto index = ::adapter::yaml::get::index(node);
+            const auto priority = priority_source.get_priority(CommandType::Value::write_single_register, index);
+            switch (operation) {
+            case (RegisterOperation::Value::clear_masked_bits):
+                return [=, mask = yaml::require_integer<uint16_t>(node, keys::mask)](ICommandSink& sink) {
+                    return sink.modify_register(index, priority, operations::clear(mask));
+                };
+            case (RegisterOperation::Value::set_masked_bits):
+                return [=, mask = yaml::require_integer<uint16_t>(node, keys::mask)](ICommandSink& sink) {
+                    return sink.modify_register(index, priority, operations::set(mask));
+                };
+            case (RegisterOperation::Value::write_value):
+                return [=, value = yaml::require_integer<uint16_t>(node, ::adapter::keys::value)](ICommandSink& sink) {
+                    return sink.set_register(index, priority, value);
+                };
+            default:
+                throw Exception("Unsupported register operation type: ", RegisterOperation::to_string(operation));
             }
         }
 
+        std::vector<sink_action_t> sink_actions(const YAML::Node& node, const ICommandPrioritySource& priority_source)
+        {
+            std::vector<sink_action_t> actions;
+
+            yaml::foreach (
+                yaml::require(node, ::adapter::keys::outputs),
+                [&actions, &priority_source](const YAML::Node& node) {
+                    const auto output_type = yaml::require_enum<OutputType>(node);
+                    switch (output_type) {
+                    case (OutputType::Value::none):
+                        break;
+                    case (OutputType::Value::write_single_register):
+                        actions.push_back(single_register_command_sink_action(node, priority_source));
+                        break;
+                    default:
+                        throw Exception("Unsupported output type: ", OutputType::to_string(output_type));
+                    }
+                });
+
+            return std::move(actions);
+        }
+
+        BinaryConfigPair binary_action_pair(const YAML::Node& node, const ICommandPrioritySource& priority_source)
+        {
+            return BinaryConfigPair{
+                std::move(
+                    sink_actions(yaml::require(node, ::adapter::keys::when_true), priority_source)),
+                std::move(
+                    sink_actions(yaml::require(node, ::adapter::keys::when_false), priority_source))
+            };
+        }
+
+        std::map<int, std::vector<sink_action_t>> enum_config(const YAML::Node& node, const google::protobuf::EnumDescriptor& descriptor, const ICommandPrioritySource& priority_source)
+        {
+            std::map<int, std::vector<sink_action_t>> map;
+
+            const auto add_entry = [&](const YAML::Node& node) {
+                const auto name = yaml::require_string(node, ::adapter::keys::name);
+                const auto value = descriptor.FindValueByName(name);
+                if (!value)
+                    throw Exception("Unknown enum value: ", name);
+                map[value->number()] = std::move(sink_actions(node, priority_source));
+            };
+
+            yaml::foreach (yaml::require(node, ::adapter::keys::mapping), add_entry);
+
+            return std::move(map);
+        }
     }
+}
 }
 
 #endif //OPENFMB_ADAPTER_COMMANDSINKACTIONS_H
--- plugins/modbus/src/modbus/SubscribeConfigReadVisitor.h
+++ plugins/modbus/src/modbus/SubscribeConfigReadVisitor.h
@@ -3,54 +3,47 @@
 #define OPENFMB_ADAPTER_MODBUS_SUBSCRIBECONFIGREADVISITOR_H
 
 #include <adapter-api/ConfigStrings.h>
-#include <adapter-api/config/ConfigReadVisitorBase.h>
+#include <adapter-api/config/SubscribingConfigReadVisitorBase.h>
 
 #include "CommandConfiguration.h"
 #include "CommandSubscriptionHandler.h"
 
+#include "CommandSinkActions.h"
 #include "ITransactionProcessor.h"
 #include "ModifyRegisterTransaction.h"
-#include "CommandSinkActions.h"
 #include "generated/OutputType.h"
-
 
 namespace adapter {
 namespace modbus {
 
     template <class T>
-    class SubscribeConfigReadVisitor final : public ConfigReadVisitorBase<T> {
+    class SubscribeConfigReadVisitor final : public SubscribingConfigReadVisitorBase<T> {
 
         const std::shared_ptr<CommandConfiguration<T>> config = std::make_shared<CommandConfiguration<T>>();
         const ICommandPrioritySource& priority_source;
-        std::string mRID;
 
     public:
         SubscribeConfigReadVisitor(const YAML::Node& root, const ICommandPrioritySource& priority_source);
 
         void subscribe(const Logger& logger, IMessageBus& bus, std::shared_ptr<ITransactionProcessor> tx_processor);
 
-        void handle(const std::string& field_name, const accessor_t<T, bool>& accessor) override;
+    protected:
+        // implement pure virtual methods from base class
 
-        void handle(const std::string& field_name, const accessor_t<T, int>& accessor) override;
+        void handle_mapped_field(const YAML::Node& node, const accessor_t<T, bool>& accessor) override;
 
-        void handle(const std::string& field_name, const accessor_t<T, long>& accessor) override;
+        void handle_mapped_field(const YAML::Node& node, const accessor_t<T, int32_t>& accessor) override;
 
-        void handle(const std::string& field_name, const accessor_t<T, float>& accessor) override;
+        void handle_mapped_field(const YAML::Node& node, const accessor_t<T, int64_t>& accessor) override;
 
-        void handle(const std::string& field_name, const accessor_t<T, std::string>& accessor) override;
+        void handle_mapped_field(const YAML::Node& node, const accessor_t<T, float>& accessor) override;
 
-        void handle(const std::string& field_name, const accessor_t<T, int>& accessor, google::protobuf::EnumDescriptor const* descriptor) override;
-
-        void handle(const std::string& field_name, const message_accessor_t<T, commonmodule::Quality>& accessor) override;
-
-        void handle(const std::string& field_name, const message_accessor_t<T, commonmodule::Timestamp>& accessor) override;
-
-        void handle(const std::string& field_name, const message_accessor_t<T, commonmodule::ControlTimestamp>& accessor) override;
+        void handle_mapped_field(const YAML::Node& node, const accessor_t<T, int>& accessor, google::protobuf::EnumDescriptor const* descriptor) override;
     };
 
     template <class T>
     SubscribeConfigReadVisitor<T>::SubscribeConfigReadVisitor(const YAML::Node& root, const ICommandPrioritySource& priority_source)
-        : ConfigReadVisitorBase<T>(root)
+        : SubscribingConfigReadVisitorBase<T>(root)
         , priority_source(priority_source)
     {
     }
@@ -58,47 +51,43 @@ namespace modbus {
     template <class T>
     void SubscribeConfigReadVisitor<T>::subscribe(const Logger& logger, IMessageBus& bus, std::shared_ptr<ITransactionProcessor> tx_processor)
     {
+
         bus.subscribe(
-            std::make_shared<CommandSubscriptionHandler<T>>(logger, this->mRID, this->config, std::move(tx_processor)));
+            std::make_shared<CommandSubscriptionHandler<T>>(logger, this->get_primary_mrid(), this->config, std::move(tx_processor)));
     }
 
     template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, bool>& accessor)
+    void SubscribeConfigReadVisitor<T>::handle_mapped_field(const YAML::Node& node, const accessor_t<T, bool>& accessor)
     {
-        const YAML::Node node = this->get_config_node(field_name);
         const auto binary_config = read::binary_action_pair(node, this->priority_source);
 
-        if(!binary_config.is_empty())
-        {
+        if (!binary_config.is_empty()) {
             this->config->add(
-                    [binary_config, accessor](const T & profile, ICommandSink & sink, Logger & logger)
-                    {
-                        accessor->if_present(
-                                profile,
-                                [&](const bool& value)
-                                {
-                                    binary_config.apply(value, sink);
-                                }
-                        );
-                    }
-            );
+                [binary_config, accessor](const T& profile, ICommandSink& sink, Logger& logger) {
+                    accessor->if_present(
+                        profile,
+                        [&](const bool& value) {
+                            binary_config.apply(value, sink);
+                        });
+                });
         }
     }
 
     template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, int32_t>& accessor)
+    void SubscribeConfigReadVisitor<T>::handle_mapped_field(const YAML::Node& node, const accessor_t<T, int32_t>& accessor)
     {
+        // ignored
     }
 
     template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, int64_t>& accessor)
+    void SubscribeConfigReadVisitor<T>::handle_mapped_field(const YAML::Node& nod, const accessor_t<T, int64_t>& accessor)
     {
+        // ignored
     }
 
     template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, float>& accessor)
+    void SubscribeConfigReadVisitor<T>::handle_mapped_field(const YAML::Node& node, const accessor_t<T, float>& accessor)
     {
-        const YAML::Node node = this->get_config_node(field_name);
         const auto output_type = yaml::require_enum<OutputType>(node);
         switch (output_type) {
         case (OutputType::Value::none):
@@ -123,28 +112,22 @@ namespace modbus {
     }
 
     template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, std::string>& accessor)
+    void SubscribeConfigReadVisitor<T>::handle_mapped_field(const YAML::Node& node, const accessor_t<T, int>& accessor, google::protobuf::EnumDescriptor const* descriptor)
     {
-    }
+        auto config = read::enum_config(node, *descriptor, this->priority_source);
 
-    template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const accessor_t<T, int>& accessor, google::protobuf::EnumDescriptor const* descriptor)
-    {
-    }
-
-    template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const message_accessor_t<T, commonmodule::Quality>& accessor)
-    {
-    }
-
-    template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const message_accessor_t<T, commonmodule::Timestamp>& accessor)
-    {
-    }
-
-    template <class T>
-    void SubscribeConfigReadVisitor<T>::handle(const std::string& field_name, const message_accessor_t<T, commonmodule::ControlTimestamp>& accessor)
-    {
+        this->config->add(
+            [config = std::move(config), accessor](const T& profile, ICommandSink& sink, Logger& logger) {
+                accessor->if_present(
+                    profile,
+                    [&](int value) {
+                        const auto entry = config.find(value);
+                        if (entry != config.end()) {
+                            for (const auto& action : entry->second)
+                                action(sink);
+                        }
+                    });
+            });
     }
 }
 }
