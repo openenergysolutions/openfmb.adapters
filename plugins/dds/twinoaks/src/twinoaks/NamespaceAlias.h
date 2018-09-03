fa3ae22f251293beb340fa4cd0f089602491777c
jadamcrain <jadamcrain@gmail.com>
2018-09-03 15:35:53 -0700

    switch to IDL from Dwayne

--- plugins/dds/twinoaks/src/twinoaks/NamespaceAlias.h
+++ plugins/dds/twinoaks/src/twinoaks/NamespaceAlias.h
@@ -3,6 +3,6 @@
 #ifndef OPENFMB_ADAPTER_NAMESPACEALIAS_H
 #define OPENFMB_ADAPTER_NAMESPACEALIAS_H
 
-namespace twinoaks = OpenFMB_Model::OpenFMB_Information_Model::openfmb;
+namespace twinoaks = ::openfmb;
 
 #endif //OPENFMB_ADAPTER_NAMESPACEALIAS_H
