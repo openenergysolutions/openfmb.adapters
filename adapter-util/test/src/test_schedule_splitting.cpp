d97428d36b3c311004deac38936573fc67237f0e
Émile Grégoire <eg@emilegregoire.ca>
2019-10-10 16:43:20 -0400

    Less duplication in ScheduleSplit, and per type timestamp validation.

--- adapter-util/test/src/test_schedule_splitting.cpp
+++ adapter-util/test/src/test_schedule_splitting.cpp
@@ -84,7 +84,7 @@ TEST_CASE("ScheduleSplit")
                 {
                     // create an islanded point
                     const auto point = schpts->Add();
-                    point->mutable_starttime()->set_seconds(now + 25);
+                    point->mutable_starttime()->set_seconds(now + 2);
                     point->mutable_starttime()->set_fraction(3);
                     const auto param = point->mutable_scheduleparameter()->Add();
                     param->set_value(105);
@@ -95,7 +95,7 @@ TEST_CASE("ScheduleSplit")
             {
                 const auto crvpts = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_esscontrolschedulefsch()->mutable_valdcsg()->mutable_crvpts();
                 const auto point = crvpts->Add();
-                point->mutable_starttime()->set_seconds(now + 26);
+                point->mutable_starttime()->set_seconds(now + 3);
                 point->mutable_starttime()->set_fraction(4);
                 point->mutable_pcthzdroop()->set_value(56.78f);
             }
@@ -112,99 +112,99 @@ TEST_CASE("ScheduleSplit")
         // there should only be two later profiles b/c 2 of the point timestamps were the same
         REQUIRE(result.later.size() == 4);
         {
-            const auto& item = result.later[0];
-            CHECK(item.first  == std::chrono::system_clock::time_point(std::chrono::seconds(now + 1)));
+            auto item = result.later.find(std::chrono::system_clock::time_point(std::chrono::seconds(now + 1)));
+            REQUIRE(item != result.later.end());
 
             // check that the conducting equip mRID is preserved
-            CHECK(item.second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
+            CHECK(item->second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
 
             // check that the message mRID is non-empty and not equal to the source
-            CHECK_FALSE(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
-            CHECK(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
+            CHECK_FALSE(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
+            CHECK(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
 
             // check the message timestamp is the time of the control
-            CHECK(item.second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 1);
+            CHECK(item->second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 1);
 
             // check the actual control value is preserved
             {
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 1);
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
 
-                auto point = *item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
+                auto point = *item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
                 CHECK(point.value() == 47.3f);
                 CHECK(point.scheduleparametertype() == commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
             }
         }
         // check two points on the same timestamp
         {
-            const auto& item = result.later[1];
-            CHECK(item.first == std::chrono::system_clock::time_point(std::chrono::seconds(now + 24)));
+            auto item = result.later.find(std::chrono::system_clock::time_point(std::chrono::seconds(now + 24)));
+            REQUIRE(item != result.later.end());
 
             // check that the conducting equip mRID is preserved
-            CHECK(item.second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
+            CHECK(item->second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
 
             // check that the message mRID is non-empty and not equal to the source
-            CHECK_FALSE(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
-            CHECK(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
+            CHECK_FALSE(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
+            CHECK(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
 
             // check the message timestamp is the time of the control
-            CHECK(item.second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 24);
+            CHECK(item->second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 24);
 
             // check the schedule control value is preserved
             {
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 2);
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 2);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
 
-                auto point = *item.second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
+                auto point = *item->second->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
                 CHECK(point.value() == 103);
                 CHECK(point.scheduleparametertype() == commonmodule::ScheduleParameterKind::ScheduleParameterKind_PhV_net_mag);
             }
         }
         // Check islanded point
         {
-            const auto& item = result.later[2];
-            CHECK(item.first == std::chrono::system_clock::time_point(std::chrono::seconds(now + 25)));
+            auto item = result.later.find(std::chrono::system_clock::time_point(std::chrono::seconds(now + 2)));
+            REQUIRE(item != result.later.end());
 
             // check that the conducting equip mRID is preserved
-            CHECK(item.second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
+            CHECK(item->second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
 
             // check that the message mRID is non-empty and not equal to the source
-            CHECK_FALSE(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
-            CHECK(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
+            CHECK_FALSE(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
+            CHECK(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
 
             // check the message timestamp is the time of the control
-            CHECK(item.second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 25);
+            CHECK(item->second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 2);
 
             // check the schedule control value is preserved
             {
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts_size() == 1);
-                REQUIRE(item.second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts().begin()->scheduleparameter_size() == 1);
 
-                auto point = *item.second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
+                auto point = *item->second->esscontrol().esscontrolfscc().controlfscc().islandcontrolschedulefsch().valacsg().schpts().begin()->scheduleparameter().begin();
                 CHECK(point.value() == 105);
                 CHECK(point.scheduleparametertype() == commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_phsC_mag);
             }
         }
         // Check custom point
         {
-            const auto& item = result.later[3];
-            CHECK(item.first == std::chrono::system_clock::time_point(std::chrono::seconds(now + 26)));
+            auto item = result.later.find(std::chrono::system_clock::time_point(std::chrono::seconds(now + 3)));
+            REQUIRE(item != result.later.end());
 
             // check that the conducting equip mRID is preserved
-            CHECK(item.second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
+            CHECK(item->second->ess().conductingequipment().mrid() == profile.ess().conductingequipment().mrid());
 
             // check that the message mRID is non-empty and not equal to the source
-            CHECK_FALSE(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
-            CHECK(item.second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
+            CHECK_FALSE(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value().empty());
+            CHECK(item->second->controlmessageinfo().messageinfo().identifiedobject().mrid().value() != profile.controlmessageinfo().messageinfo().identifiedobject().mrid().value());
 
             // check the message timestamp is the time of the control
-            CHECK(item.second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 26);
+            CHECK(item->second->controlmessageinfo().messageinfo().messagetimestamp().seconds() == now + 3);
 
             // check the schedule control value is preserved
             {
-                REQUIRE(item.second->esscontrol().esscontrolfscc().esscontrolschedulefsch().valdcsg().crvpts_size() == 1);
+                REQUIRE(item->second->esscontrol().esscontrolfscc().esscontrolschedulefsch().valdcsg().crvpts_size() == 1);
 
-                auto point = *item.second->esscontrol().esscontrolfscc().esscontrolschedulefsch().valdcsg().crvpts().begin();
+                auto point = *item->second->esscontrol().esscontrolfscc().esscontrolschedulefsch().valdcsg().crvpts().begin();
                 CHECK(point.pcthzdroop().value() == 56.78f);
             }
         }
@@ -277,6 +277,96 @@ TEST_CASE("ScheduleSplit")
         REQUIRE_THROWS_WITH(splitter.split(profile), "Schedule points not in order");
     }
 
+    SECTION("points in order across different types")
+    {
+        essmodule::ESSControlProfile profile;
+        // setup the metadata
+        profile.mutable_ess()->mutable_conductingequipment()->set_mrid(boost::uuids::to_string(rg()));
+        profile.mutable_controlmessageinfo()->mutable_messageinfo()->mutable_messagetimestamp()->set_seconds(now);
+        profile.mutable_controlmessageinfo()->mutable_messageinfo()->mutable_identifiedobject()->mutable_mrid()->set_value(boost::uuids::to_string(rg()));
+
+        {
+            const auto points = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_controlfscc()->mutable_controlschedulefsch()->mutable_valacsg()->mutable_schpts();
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now);
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(47.3f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now + 20);
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(47.3f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now + 40); // Not in order!
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(34.7f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+        }
+
+        {
+            const auto points = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_controlfscc()->mutable_islandcontrolschedulefsch()->mutable_valacsg()->mutable_schpts();
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now + 1);
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(47.3f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now + 20);
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(47.3f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+            {
+                const auto point = points->Add();
+                point->mutable_starttime()->set_seconds(now + 40); // Not in order!
+                const auto param = point->mutable_scheduleparameter()->Add();
+                param->set_value(34.7f);
+                param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+            }
+        }
+
+        REQUIRE_NOTHROW(splitter.split(profile));
+    }
+
+    SECTION("points of other type doesn't have now point")
+    {
+        essmodule::ESSControlProfile profile;
+        // setup the metadata
+        profile.mutable_ess()->mutable_conductingequipment()->set_mrid(boost::uuids::to_string(rg()));
+        profile.mutable_controlmessageinfo()->mutable_messageinfo()->mutable_messagetimestamp()->set_seconds(now);
+        profile.mutable_controlmessageinfo()->mutable_messageinfo()->mutable_identifiedobject()->mutable_mrid()->set_value(boost::uuids::to_string(rg()));
+
+        {
+            const auto points = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_controlfscc()->mutable_controlschedulefsch()->mutable_valacsg()->mutable_schpts();
+            const auto point = points->Add();
+            point->mutable_starttime()->set_seconds(now);
+            const auto param = point->mutable_scheduleparameter()->Add();
+            param->set_value(47.3f);
+            param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+        }
+
+        {
+            const auto points = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_controlfscc()->mutable_islandcontrolschedulefsch()->mutable_valacsg()->mutable_schpts();
+            const auto point = points->Add();
+            point->mutable_starttime()->set_seconds(now + 20);
+            const auto param = point->mutable_scheduleparameter()->Add();
+            param->set_value(47.3f);
+            param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
+        }
+
+        REQUIRE_THROWS(splitter.split(profile), "Schedule points not in order");
+    }
+
     SECTION("first point outside of tolerance (after)")
     {
         essmodule::ESSControlProfile profile;
@@ -335,7 +425,7 @@ TEST_CASE("ScheduleSplit")
         REQUIRE_THROWS_WITH(splitter.split(profile), "Schedule point does not have a value associated with a timestamp");
     }
 
-    SECTION("first value a little later is put in later")
+    SECTION("first value before now is put in now")
     {
         essmodule::ESSControlProfile profile;
         // setup the metadata
@@ -346,14 +436,14 @@ TEST_CASE("ScheduleSplit")
         const auto points = profile.mutable_esscontrol()->mutable_esscontrolfscc()->mutable_controlfscc()->mutable_controlschedulefsch()->mutable_valacsg()->mutable_schpts();
         {
             const auto point = points->Add();
-            point->mutable_starttime()->set_seconds(now + 1);
+            point->mutable_starttime()->set_seconds(now - 1);
             const auto param = point->mutable_scheduleparameter()->Add();
             param->set_value(47.3f);
             param->set_scheduleparametertype(commonmodule::ScheduleParameterKind::ScheduleParameterKind_A_net_mag);
         }
 
         const auto result = splitter.split(profile);
-        CHECK(result.now->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 0);
-        CHECK(result.later.size() == 1);
+        CHECK(result.now->esscontrol().esscontrolfscc().controlfscc().controlschedulefsch().valacsg().schpts_size() == 1);
+        CHECK(result.later.size() == 0);
     }
 }
