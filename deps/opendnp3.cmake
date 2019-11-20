fb495262284a5b81c67b619f5d42d2114d239f92
Émile Grégoire <eg@emilegregoire.ca>
2019-11-20 12:41:23 -0500

    Specify network adapter in Modbus.

--- deps/opendnp3.cmake
+++ deps/opendnp3.cmake
@@ -2,9 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     opendnp3
-    GIT_REPOSITORY https://github.com/dnp3/opendnp3.git
-    GIT_TAG        develop
-    GIT_SHALLOW    ON
+    URL      https://github.com/dnp3/opendnp3/archive/c1e43b9b38094f222af04cba31dcffb9bdb95bf9.zip
+    URL_HASH SHA1=91DBC4D504925ABFAF2D25FD6FA2328F65262478
 )
 
 FetchContent_GetProperties(opendnp3)
