f628a6e10ab83339e9106850986b0a5789f1f38e
Tom Burdick <thomas.burdick@gmail.com>
2020-09-04 09:41:29 -0500

    Fix missing $ in circleci config for arm
    Sets compile flags to optimize for size on arm

--- cmake/Armv7HfToolchain.cmake
+++ cmake/Armv7HfToolchain.cmake
@@ -4,7 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
-set(flags "-march=armv7 -mfpu=neon-vfpv3 -mfloat-abi=hard")
+set(flags "-march=armv7 -mfpu=neon-vfpv3 -mfloat-abi=hard -Os")
 set(CMAKE_C_FLAGS ${flags})
 set(CMAKE_CXX_FLAGS ${flags})
 set(CMAKE_EXE_LINKER_FLAGS ${flags})
