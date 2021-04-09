5e47e78dd253fcd47a54ea5409b7b4e7630c07cd
Tom Burdick <thomas.burdick@gmail.com>
2021-04-09 12:55:41 -0500

    Update our pinned nixpkgs to fix several CVEs
    Specifically OpenSSL related CVEs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -8,7 +8,7 @@ import (builtins.fetchTarball {
   # Commit hash for nixos-20.09
   # Obtained from https://channels.nix.gsc.io/nixos-20.09/latest-v2
   # with patches applied to reduce gcc+musl closure sizes
-  url = "https://github.com/openenergysolutions/nixpkgs/archive/d6e6908dbf37ae0ae131de2eeeb6985f26dbaaf7.tar.gz";
+  url = "https://github.com/openenergysolutions/nixpkgs/archive/54e518a43e8fb5ea7ecdc164f8c98bb40b2a7027.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "0chclr5h4mxyxjj1r22jaqp0cwswrdg1fpymmgn2jryqsv7mjdky";
+  sha256 = "0g5ng0x82vhkr1f3bzszhjm98n6q5cy6dvj1bha55imrqi7r7v8w";
 })
