8ad9fd9c0e529526a9dc008cbd4ea24a9f720300
Cory Nguyen <cory@openenergysolutionsinc.com>
2021-10-19 12:39:41 -0400

    conan package manager fixed zenoh subject
    Signed-off-by: Cory Nguyen <cory@openenergysolutionsinc.com>

--- deps/exe4cpp.cmake
+++ deps/exe4cpp.cmake
@@ -2,21 +2,23 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-#include(FetchContent)
-#
-#FetchContent_Declare(
-#    exe4cpp
-#    URL      https://github.com/automatak/exe4cpp/archive/fb878a4de598ba9d6e4338afebf83f96e03af1b8.zip
-#    URL_HASH SHA1=18B141E8CF09DC8D28CC62DD5FA2920670D501BD
-#)
-#
-#FetchContent_GetProperties(exe4cpp)
-#if(NOT exe4cpp_POPULATED)
-#    FetchContent_Populate(exe4cpp)
-#    add_subdirectory(${exe4cpp_SOURCE_DIR})
-#endif()
+if(OPENFMB_USE_CONAN)
+    include(FetchContent)
 
-find_path(EXE4CPP_INCLUDE exe4cpp/Typedefs.h REQUIRED)
-add_library(exe4cpp INTERFACE)
-target_include_directories(exe4cpp INTERFACE ${EXE4CPP_INCLUDE})
-target_compile_features(exe4cpp INTERFACE cxx_std_14)
+    FetchContent_Declare(
+        exe4cpp
+        URL      https://github.com/automatak/exe4cpp/archive/fb878a4de598ba9d6e4338afebf83f96e03af1b8.zip
+        URL_HASH SHA1=18B141E8CF09DC8D28CC62DD5FA2920670D501BD
+    )
+
+    FetchContent_GetProperties(exe4cpp)
+    if(NOT exe4cpp_POPULATED)
+        FetchContent_Populate(exe4cpp)
+        add_subdirectory(${exe4cpp_SOURCE_DIR})
+    endif()
+else()
+    find_path(EXE4CPP_INCLUDE exe4cpp/Typedefs.h REQUIRED)
+    add_library(exe4cpp INTERFACE)
+    target_include_directories(exe4cpp INTERFACE ${EXE4CPP_INCLUDE})
+    target_compile_features(exe4cpp INTERFACE cxx_std_14)
+endif()
--- deps/modbus-cpp.cmake
+++ deps/modbus-cpp.cmake
@@ -2,14 +2,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-if(ADAPTER_VENDORED_DEPS)
+if(OPENFMB_USE_CONAN)
     include(FetchContent)
 
     FetchContent_Declare(
         modbus-cpp
         GIT_REPOSITORY git@github.com:openenergysolutions/modbus-cpp.git
         GIT_TAG        669b3e47545faa71816c685276f7cdcf64629f9a
-        )
+    )
 
     FetchContent_GetProperties(modbus-cpp)
     if(NOT modbus-cpp_POPULATED)
@@ -17,5 +17,21 @@ if(ADAPTER_VENDORED_DEPS)
         add_subdirectory(${modbus-cpp_SOURCE_DIR})
     endif()
 else()
-    find_package(modbuscpp REQUIRED)
+    if(ADAPTER_VENDORED_DEPS)
+        include(FetchContent)
+
+        FetchContent_Declare(
+            modbus-cpp
+            GIT_REPOSITORY git@github.com:openenergysolutions/modbus-cpp.git
+            GIT_TAG        669b3e47545faa71816c685276f7cdcf64629f9a
+            )
+
+        FetchContent_GetProperties(modbus-cpp)
+        if(NOT modbus-cpp_POPULATED)
+            FetchContent_Populate(modbus-cpp)
+            add_subdirectory(${modbus-cpp_SOURCE_DIR})
+        endif()
+    else()
+        find_package(modbuscpp REQUIRED)
+    endif()
 endif()
